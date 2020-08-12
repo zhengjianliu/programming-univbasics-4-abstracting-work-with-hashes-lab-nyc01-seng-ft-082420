8d2c99c6ec446713e32eaabc09b3d550f48a8e33
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-12 00:58:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,7 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  hash[key]
+  return hash[key]
 end
 
 def update_counting_hash(hash, key)
@@ -17,5 +17,5 @@ def update_counting_hash(hash, key)
   else
     hash[key] = 1
   end
-  hash
+  return hash
 end
